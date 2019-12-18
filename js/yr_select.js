0f1e50f90f3285f2b4bcb5f1d96c4ef9f0c825f0
David Hamilton <dave@davemail.ca>
2019-12-18 14:45:08 -0500

    Draft 1

--- js/yr_select.js
+++
@@ -1,9 +0,0 @@
-function fun_a() {
-  document.getElementById("2019").style.display = "block";
-  document.getElementById("2018").style.display = "none";
-}
-
-function fun_b() {
-  document.getElementById("2019").style.display = "none";
-  document.getElementById("2018").style.display = "block";
-}
