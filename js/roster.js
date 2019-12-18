605bb0b51985b17a4bc84a28477d130058e5ae2f
David Hamilton <dave@davemail.ca>
2019-12-18 16:53:30 -0500

    Draft 1.5 - basic UI

--- js/roster.js
+++ js/roster.js
@@ -4,10 +4,30 @@ function openClose(cl_name) {
     if (x[0].getAttribute('style') == null || x[0].getAttribute('style') == 'display: none;'){
       x[0].style.display="inline-block";
       x[1].style.display="inline-block";
+      x[2].style.display="inline-block";
     }
     else {
       x[0].style.display="none";
       x[1].style.display="none";
+      x[2].style.display="none";
+    }
+  }
+}
+
+function openClose4(cl_name) {
+  if ((document.documentElement.clientWidth) < '800'){
+    x=document.getElementsByClassName(cl_name);
+    if (x[0].getAttribute('style') == null || x[0].getAttribute('style') == 'display: none;'){
+      x[0].style.display="inline-block";
+      x[1].style.display="inline-block";
+      x[2].style.display="inline-block";
+      x[3].style.display="inline-block";
+    }
+    else {
+      x[0].style.display="none";
+      x[1].style.display="none";
+      x[2].style.display="none";
+      x[3].style.display="none";
     }
   }
 }
